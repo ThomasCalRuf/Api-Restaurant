0f3aa8f52739edc287f83b36ee2e984e68b361a8
ThomasCalRuf <thomas.calvete@hotmail.com>
2023-03-21 21:45:43 +0100

    Ajout de la route Fin de service

--- api/controllers/serviceController.js
+++ api/controllers/serviceController.js
@@ -1,5 +1,7 @@
 const Service = require('../models/serviceModel');
 const ServiceUser = require('../models/serviceUsersModel');
+const TableTips = require("../models/tableTipsModel");
+const TipsPayments = require('../models/tipsPaymentsModel');
 
 exports.listAllServices = async (req,res)=>{
     try {
@@ -17,7 +19,7 @@ exports.listAllServices = async (req,res)=>{
 
 exports.createService = async (req,res)=>{
     const { shiftType, shiftClosed, usersid } = req.body;
-
+    
     try {
         const service = await Service.create({  shiftType, shiftClosed });
         const id_service = service.dataValues.id;
@@ -97,4 +99,37 @@ exports.deleteAService = async (req,res)=>{
         res.status(500);
         res.json({ message: 'Un problème est arrivé lors de la suppression du service.' });
     }
+}
+
+exports.endService = async (req,res)=>{
+    const { id } = req.params; 
+    let count=0;
+    try {
+        const service = await Service.findByPk(id);
+        if (service) {
+            service.dataValues.shiftClosed = true;
+            await service.save();
+            const alltips = await TableTips.sum('tips',{where: {id_service: id}});
+            const alluser = await ServiceUser.findAll({where: {id_service : id}});
+            alluser.forEach(() =>{
+                count++;
+            })
+            let amount = alltips/count;
+            alluser.forEach(async id =>{
+                const user = await TipsPayments.findOne({where: {id_user: id.dataValues.id_user}});
+                user.amount = amount;
+                console.log("jjjjj",user);
+                await user.save();
+            })
+            res.status(200);
+            res.json(("C'est bon"));
+        } else {
+            res.status(404);
+            res.json({ message: 'Service non trouvé.' });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500);
+        res.json({ message: 'Un problème est arrivé lors de la fermeture du service.' });
+    }  
 }
